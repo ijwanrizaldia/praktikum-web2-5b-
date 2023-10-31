482a1d0e62b47f2b8e5293c07f10a95677f960c2
ijwanrizaldia <rizaldipc@gmail.com>
2023-10-31 17:43:20 +0800

    upload ketiga

--- application/models/Surat_model.php
+++ application/models/Surat_model.php
@@ -1,5 +1,5 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
-class Masuk_model extends CI_Model
+class Surat_model extends CI_Model
 {
  protected $_table ='tb_surat_masuk'; 
  protected $primary = 'id';
